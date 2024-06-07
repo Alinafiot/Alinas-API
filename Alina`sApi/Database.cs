e5b78025a21508237ebb569588ae435d4f04e005
Xxaora <162331399+Alinafiot@users.noreply.github.com>
2024-06-07 11:38:24 +0300

    Add files via upload

---
+++ Alina`sApi/Database.cs
@@ -0,0 +1,58 @@
+﻿using Npgsql;
+using System.Threading.Tasks;
+
+namespace Alina_sApi
+{
+    public class Database
+    {
+        NpgsqlConnection con = new NpgsqlConnection(Constants.Connect);
+
+        public async Task InsertBookAsync(string title)
+        {
+            var sql = "insert into public.\"RatingBooks\"(\"Title\")"
+                    + "values (@Title)";
+
+            NpgsqlCommand comm = new NpgsqlCommand(sql, con);
+            comm.Parameters.AddWithValue("Title", title);
+
+            await con.OpenAsync();
+            await comm.ExecuteNonQueryAsync();
+            await con.CloseAsync();
+        }
+        public async Task<bool> DeleteReviewForBookAsync(string title)
+        {
+            var sql = "delete from public.\"RatingBooks\" where \"Title\" = @Title";
+
+            NpgsqlCommand comm = new NpgsqlCommand(sql, con);
+            comm.Parameters.AddWithValue("Title", title);
+
+            await con.OpenAsync();
+            var rowsAffected = await comm.ExecuteNonQueryAsync();
+            await con.CloseAsync();
+
+            return rowsAffected > 0;
+        }
+
+        public async Task<List<string>> GetBooksAsync()
+        {
+            var books = new List<string>();
+            var sql = "select \"Title\" from public.\"RatingBooks\"";
+
+            NpgsqlCommand comm = new NpgsqlCommand(sql, con);
+
+            await con.OpenAsync();
+            var reader = await comm.ExecuteReaderAsync();
+
+            while (await reader.ReadAsync())
+            {
+                books.Add(reader.GetString(0));
+            }
+
+            await reader.CloseAsync();
+            await con.CloseAsync();
+
+            return books;
+        }
+
+    }
+}
