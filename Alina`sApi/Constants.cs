96991e830dc9c528aab50f059be53651ae66b85e
Xxaora <162331399+Alinafiot@users.noreply.github.com>
2024-06-07 11:39:02 +0300

    Update Constants.cs

--- Alina`sApi/Constants.cs
+++ Alina`sApi/Constants.cs
@@ -1,8 +1,9 @@
-﻿namespace Alina_sApi
+namespace Alina_sApi
 {
     public class Constants
     {
         public static string Address = "https://www.googleapis.com/books/v1/volumes";
         public static string Apikey = "AIzaSyAa8Vg2A4By1n1naBdOr8-nsE343SC6RpE";
+        public static string Connect = "Host=localhost;Username=postgres;Password=7889alina;Database=postgres";
     }
 }
